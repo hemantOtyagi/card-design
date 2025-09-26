8e1e01986d13aa14169172c075197d64d7a070b9
hemant <tyagi26326@gmail.com>
2025-09-26 23:56:03 +0530

    added neumorphcard

---
+++ src/components/animations/neumorphism-card.jsx
@@ -0,0 +1,9 @@
+const Card = () => {
+  return (
+    <div className="h-32 text-black text-xs flex justify-center items-center w-72 py-4 my-10 bg-[#e6e9f3] rounded-lg shadow-[inset_0rem_0.2rem_0.4rem_0_rgb(0,0,0,0.1)] hover:shadow-[inset_0rem_0.2rem_0.4rem_0_rgb(255,255,255,0.5)] transition-shadow duration-300">
+      Neumorphism Card 
+    </div>
+  )
+}
+
+export default Card 
