7b697cc1d234f2d408cba78cb82d62f2f2d2667c
hemant <tyagi26326@gmail.com>
2025-10-01 18:09:42 +0530

    added batch card

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import Card from "./components/animations/neumorphism-card"
-import BatchDialog from "./components/batch"
+import EventForm from "./components/batch"
 
 const App = () => {
   return (
     <div className="h-screen w-screen flex justify-center items-center bg-white">
       {/* <Card/> */}
-      <BatchDialog/>
+      <EventForm/>
     </div>
   )
 }
